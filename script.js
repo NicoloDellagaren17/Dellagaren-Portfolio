f3cdc11f31ef57a47b8803e0b1d287dac41d33bb
NicoloDellagaren17 <153521154+NicoloDellagaren17@users.noreply.github.com>
2024-09-03 17:58:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
 let currentIndex = 0;
 let autoSlideInterval;
 
-let slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.slide');
 const prev = document.getElementById('prevSlide');
-prev.addEventListener('click', prevSlide)
-
 const next = document.getElementById('nextSlide');
-next.addEventListener('click', nextSlide)
 
-startAutoSlide(); // Avvia lo scorrimento automatico all'avvio
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
 
-function showSlide(index) {            
-const dots = document.querySelectorAll('.dot');
+startAutoSlide();
 
-  if (index < 0) {
-         index = slides.length - 1;
+function showSlide(index) {
+    const dots = document.querySelectorAll('.dot');
+
+    if (index < 0) {
+        index = slides.length - 1;
     } else if (index >= slides.length) {
-         index = 0;
+        index = 0;
     }
 
     currentIndex = index;
 
     slides.forEach((slide, i) => {
-         slide.classList.toggle('active', i === index);
-     });
+        slide.classList.toggle('active', i === index);
+    });
 
-     dots.forEach((dot, i) => {
-          dot.classList.toggle('active', i === index);
-       });
-  }
+    dots.forEach((dot, i) => {
+        dot.classList.toggle('active', i === index);
+    });
+}
 
 function nextSlide() {
-      stopAutoSlide();
-      showSlide(currentIndex + 1);
- }
+    stopAutoSlide();
+    showSlide(currentIndex + 1);
+}
 
 function prevSlide() {
-       stopAutoSlide();
-       showSlide(currentIndex - 1);
+    stopAutoSlide();
+    showSlide(currentIndex - 1);
 }
 
 function startAutoSlide() {
-      autoSlideInterval = setInterval(() => {
-          nextSlide();
-        }, 3000);
- }
-
-function stopAutoSlide() {
-       clearInterval(autoSlideInterval);
+    autoSlideInterval = setInterval(() => {
+        nextSlide();
+    }, 3000);
 }
 
-// Creazione dinamica dei punti di navigazione
-      
+function stopAutoSlide() {
+    clearInterval(autoSlideInterval);
+}
+
 const dotsContainer = document.getElementById('dots-container');
 
 for (let i = 0; i < slides.length; i++) {
-       const dot = document.createElement('span');
-       dot.classList.add('dot');
-       dot.addEventListener('click', () => {
-             showSlide(i);
-             stopAutoSlide();
-       });
-       dotsContainer.appendChild(dot);
+    const dot = document.createElement('span');
+    dot.classList.add('dot');
+    dot.addEventListener('click', () => {
+        showSlide(i);
+        stopAutoSlide();
+    });
+    dotsContainer.appendChild(dot);
+}
+
  } 
